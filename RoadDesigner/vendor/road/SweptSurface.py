062eb70966eeb0dcb0a7342553e081d4b2c02481
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-03-12 22:41:24 +0000

    test,data:straight.toml:Improve precision of profile texture coordinates feat:SweptSurface.py:Add texture coordinates using length, width and initial texture coordinates as a guide.

--- RoadDesigner/vendor/road/SweptSurface.py
+++ RoadDesigner/vendor/road/SweptSurface.py
@@ -3,11 +3,13 @@ try:
     from .vendor.road import Polyline
     from .vendor.road import Segment
     from .vendor.road import Vertex
+    from .vendor.pyglm import glm
 except ImportError:
     import Quad
     import Polyline
     import Segment
     import Vertex
+    from pyglm import glm
 
 class SweptSurface:
     def __init__(self, profile:list[Quad.Quad], axis:Segment.Segment):
@@ -23,13 +25,20 @@ class SweptSurface:
         # for each point in the axis do:
         #   generate a quad
         for quad in self.profile:
+            width = glm.length(quad.verts[1].position - quad.verts[0].position)
+            length = glm.length(self.axis.points[1] - self.axis.points[0])
+            real_world_size = glm.vec2(width/quad.verts[1].tex_coord.x, quad.verts[2].position.y / quad.verts[2].tex_coord.y)
             self.points.append(quad.verts[0])
             self.points.append(quad.verts[1])
             self.points.append(Vertex.Vertex())
             self.points[-1].position.x = quad.verts[2].position.x
             self.points[-1].position.y = self.axis.points[1].y
+            self.points[-1].tex_coord.x = quad.verts[2].tex_coord.x
+            self.points[-1].tex_coord.y = length / real_world_size.y
             self.points.append(Vertex.Vertex())
             self.points[-1].position.x = quad.verts[3].position.x
             self.points[-1].position.y = self.axis.points[1].y
+            self.points[-1].tex_coord.x = width / real_world_size.x
+            self.points[-1].tex_coord.y = length / real_world_size.y
 
 
